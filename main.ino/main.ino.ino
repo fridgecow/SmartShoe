45de5331277f166f19936582586f99bd01c8134d
fridgecow <tcbenn@fridgecow.com>
2015-08-10 00:48:45 +0100

    First working build. Includes heelclick interface and a basic compass.
    To fix: delay on heelclick LED, compass doesn't act continuously because
    0 and 360 are not numerically next to each other

---
+++ main.ino/main.ino.ino
@@ -0,0 +1,118 @@
+#include <Wire.h>
+#include <LSM303.h>
+
+#include <SPI.h>
+#include <Adafruit_BLE.h>
+#include <Adafruit_BluefruitLE_UART.h>
+#include <Adafruit_NeoPixel.h>
+
+#define MODES 1
+
+Adafruit_BluefruitLE_UART ble(Serial1, 12); //BLE module
+LSM303 lsm; //Accelerometer + Magnetometer
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(4, 10, NEO_GRB + NEO_KHZ800); //NeoPixels
+
+//Global variables
+int mode = 0; //Default mode (off)
+int activate = 0; //Number of 'heelclicks' registered.
+unsigned long schmittTimer = 0; //Prevents repeated heel clicks.
+uint32_t PixelArray[] = {0,0,0,0}; //Caches the current state so that a 'refresh' doesn't always happen
+bool pixelChanged = false; //Have the pixels *actually* been changed.
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Try to init the accelerometer
+  lsm.init();
+  lsm.enableDefault();
+  
+  //Default calibration values
+  lsm.m_min = (LSM303::vector<int16_t>){  -492,   -947,   -402};
+  lsm.m_max = (LSM303::vector<int16_t>){  +534,   +182,   +538}
+;
+  
+  //Try to init BLE
+  if (!ble.begin(true)){
+    Serial.println("Couldn't find Bluefruit, make sure it's in COM mode & check wiring?");
+  }
+  
+  //Try to init neopixels
+  pixels.begin();
+
+  /* Perform a factory reset of BLE to make sure everything is in a known state */
+  Serial.println(F("Performing a factory reset: "));
+  if (! ble.factoryReset() ){
+       Serial.println(F("Couldn't factory reset"));
+  }
+
+  /* Disable command echo from Bluefruit */
+  ble.echo(false);
+  
+  Serial.println("OK!, hardware ready!");
+}
+
+void setPixel(uint16_t n, uint32_t colour){
+  if(PixelArray[n] == colour){ //No change
+    return;
+  }else{ //Change
+    pixels.setPixelColor(n, colour);
+    PixelArray[n] = colour;
+    pixelChanged = true;
+    Serial.println("Pixel Changed");
+  }
+}
+
+void loop() {
+  lsm.read();
+  float head = lsm.heading(); //Get heading from magnetometer
+  //ble.print("\n");
+  
+  //Compass functionality
+  if(mode == 0 && (millis() - schmittTimer) > 500){ //Not active - clear screen
+    for(int pixel = 0; pixel < 4; pixel++){
+      setPixel(pixel, pixels.Color(0,0,0));
+    }
+  }else if(mode == 1){
+    for(int pixel = 0; pixel < 4; pixel++){ //Loop through all pixels
+      float angle = (3 - pixel)*90; //Angle of this pixel - reversed as the pixels were wired ccw.
+      float diff = abs(head - angle); //Difference between this pixel and our heading
+      //Calculate 0 <= brightness <= 1
+      float brightness = (float)(90 - diff)/(float)90;
+      if (brightness < 0){
+        brightness = 0;
+      }
+      setPixel(pixel, pixels.Color(0,(int)(255*brightness), 0));
+    }
+  }
+  
+  //Heel clicks
+  if(lsm.a.x < 0 && lsm.a.z > -11000 && (millis() - schmittTimer) > 500){ //Significant 'outward' rotation, lifted off ground - yes, a gyroscope would be better
+    if(mode == 0){ //Not active
+      if((millis() - schmittTimer) > 5000){ //5 second reset.
+        activate = 0;
+      }
+      if(activate == 2){ //This is the third time
+        mode = 1;
+        Serial.println("Active!");
+        activate = 0;
+      }
+      activate++;
+      for(int p = 0; p < activate; p++){
+        setPixel(p, pixels.Color(255, 0, 0));
+      }
+    }else{ //Active
+      mode++;
+      if(mode > MODES){
+        mode = 0;
+        activate = 2; //Give a 5 second reset.
+      }
+    }
+    schmittTimer = millis(); //Reset timer
+    Serial.println("Heel Click!");
+  }
+  //Output
+  if(pixelChanged){
+    pixels.show();
+    pixelChanged = false;
+  }
+}
